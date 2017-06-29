5410d6458bd6f1b48456af09e1df16ad9b7dd10f
Vivhchj <641187840@qq.com>
2017-06-30 00:41:55 +0800

    Add files via upload

---
+++ message_1/js/time_calculation.js
@@ -0,0 +1,44 @@
+    Function.prototype.before=function(func){
+	    var self=this;
+		return function(){
+		if(func.apply(this,arguments)===false)
+		{	
+			return false;
+		}
+		return self.apply(this,arguments);	
+		}
+ 
+    }; 
+	Function.prototype.after=function(func){
+		var self=this;
+		return function(){
+		var te=self.apply(this,arguments);
+		if(te===false)
+		{	
+			return false;
+		}
+		func.apply(this,arguments);
+		return te;
+		}
+ 
+    }; 
+    function doCircle(){// 事件函数，指定次数的循环
+ 
+        var total = 0; 
+        for(var i=1;i<=10000000;i++){ 
+            total += 1; 
+        } 
+		for(var i=1;i<=10000000;i++){ 
+            total -= 1; 
+        } 
+                
+    } 
+	
+    $(document).ready(function(){ 
+	    var beforetime;
+	    doCircle=doCircle.before(function(){//设置切点
+	        beforetime = new Date().getTime();// 记录当前时间
+	    }).after(function(){
+	        alert((new Date().getTime() - beforetime) + " ms");	// 得到执行时间
+        }); 
+    }); 
